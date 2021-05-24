bdf1e4e7844cd861a9f5b93bb195fe675eee32a1
Gideon Opoku <36802637+nana562@users.noreply.github.com>
2021-05-24 16:05:09 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ const app = express();
 
 app.use(cors());
 
+
 app.get('/:location', (req, res)=>{
   const location = req.params.location
   axios.get(`${process.env.API_URL}${location}&units=${process.env.API_UNIT}&APPID=${process.env.API_KEY}`)
     .then((response)=>{
-      res.send(response.data)
+      res.json(response.data)
     })
 })
 
-app.get('/test', (req, res)=>{
-  res.send("Endpoint working")
-})
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
